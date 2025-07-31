3155fe079ee73d1cbc759dc921877a27efd8be2e
nandinidas07 <nandinidas107@gmail.com>
2025-07-31 20:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let messages = [];
+
+function submitMessage() {
+  const input = document.querySelector('input');
+  const output = document.getElementById('output');
+  const message = input.value.trim();
+
+  if (message) {
+    messages.push(message); 
+    input.value = ''; 
+  }
+
+  if (messages.length > 0) {
+    const msg = messages.shift(); 
+    output.innerText = msg;
+
+    setTimeout(() => {
+      output.innerText = '';
+    }, 2000);
+  }
+}
+
+document.getElementById('submitBtn').addEventListener('click', submitMessage);
